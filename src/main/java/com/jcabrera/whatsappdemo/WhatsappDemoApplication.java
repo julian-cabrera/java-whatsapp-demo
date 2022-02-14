ea692fbe6af347a60417ca15b594c05748e4a777
Julián Cabrera <julian.e.cabrera@hotmail.com>
2022-02-14 18:23:54 -0300

    first commit

---
+++ src/main/java/com/jcabrera/whatsappdemo/WhatsappDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jcabrera.whatsappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jcabrera/whatsappdemo/model/Message.java
@@ -0,0 +1,23 @@
+package com.jcabrera.whatsappdemo.model;
+
+public class Message {
+  private String id;
+  private Boolean sent;
+  private String body;
+  private String type;
+  private String senderName;
+  private Boolean fromMe;
+  private String author;
+  private Integer time;
+  private String chatId;
+  private String mentionedPhones;
+  private String quotedMsgId;
+  private String quotedMsgBody;
+  private Integer phone;
+  private Integer messageNumber;
+  private Integer queueNumber;
+  private String filename;
+  private String caption;
+  private Boolean cached;
+  private String audio;
+}
